8b072cb13146655bb47f53d61d17589bffc52f7e
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2024-12-28 17:10:42 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
+
+// const response = await fetch('https://script.google.com/macros/s/AKfycbycBtbkg_d7af6M8DqCsvIF1IbxTlYiWmHsyy8pA1bdwrZt_1GO5SWVm0aO12QDd61u/exec');
